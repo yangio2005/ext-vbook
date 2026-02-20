cf9391f3ee51dec55844ef2d2d49b47340387e3a
aczaao12 <aczaao12@gmail.com>
2026-02-20 20:52:21 +0700

    Refactor: Move all files to root directory

--- vbook-extensions/tiemtruyenchu/src/config.js
+++
@@ -1,8 +0,0 @@
-var BASE_URL = 'https://www.tiemtruyenchu.com';
-try {
-    if (typeof CONFIG_URL !== 'undefined' && CONFIG_URL) {
-        BASE_URL = CONFIG_URL;
-    }
-} catch (error) {
-    // Bỏ qua lỗi
-}
--- vbook-extensions/tiemtruyenchu/src/home.js
+++
@@ -1,9 +0,0 @@
-load('config.js');
-function execute() {
-    return Response.success([
-        { title: "Mới cập nhật", input: BASE_URL + "/danh-sach?sort=updated", script: "gen.js" },
-        { title: "Truyện mới", input: BASE_URL + "/danh-sach?sort=new", script: "gen.js" },
-        { title: "Hoàn thành", input: BASE_URL + "/danh-sach?status=full", script: "gen.js" },
-        { title: "Sáng tác", input: BASE_URL + "/danh-sach?type=sang-tac", script: "gen.js" },
-    ]);
-}
